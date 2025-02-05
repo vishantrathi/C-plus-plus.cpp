7cb043a4d5509133b53ab615e76e78c1b1c53b25
vishantrathi <vishantrathigurjar@gmail.com>
2025-02-05 23:15:30 +0530

    #

--- DSA/STLlist.cpp
+++ DSA/STLlist.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
-#include<vector>
+#include<list>
+using namespace std;
 int main(){
     list<int>ls;
     ls.push_back(2);
     ls.emplace_back(4);
     ls.push_front(5);
-    ls.emplace_fromt(7);
-    cout<<ls;
+    ls.emplace_front(7);
 
+    for (int i : ls) {
+        cout << i << " "<<endl;
+    }
 
+    return 0;
 }
