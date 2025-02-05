aa2caf71501ba68e3bb272dcd9b011ffe1fcadbd
vishantrathi <vishantrathigurjar@gmail.com>
2025-02-05 22:58:36 +0530

    #

--- DSA/STLVector.cpp
+++ DSA/STLVector.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-
-    void vector(){
+    int main(){
     vector<int>v;
-    v.push_back(10);
+    v.push_back(1);
     v.emplace_back(2);
+
+    for(int i:v){
+        cout<<i<<" ";
     }
     return 0;
+    }
