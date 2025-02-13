3569f9956e37c44eb095470e3a6535e44120372d
vishantrathi <vishantrathigurjar@gmail.com>
2025-02-13 23:58:24 +0530

    #

--- DSA/Palindrom.cpp
+++ DSA/Palindrom.cpp
@@ -5,4 +5,21 @@ int main()
 {
     
     return 0;
-}
+}int main() {
+    1
+    int n;
+    cin >> n •
+    int revNum
+    int dup = n;
+    while(n>()) {
+    int ld
+    = nuo;
+    revNum =
+    ( revNum *
+    10)
+    + ld;
+    " true"
+    if (dup
+    else cout
+    revNum) cout
+    "false"
