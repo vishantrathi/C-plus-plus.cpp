961ddf427cd2f1f6dd8664dd3998b0f026fa23b6
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-02-23 19:56:41 +0530

    #

---
+++ DSA/set.cpp
@@ -0,0 +1,15 @@
+void set(){
+    st.insert(1);
+    st.insert(2);
+    st.insert(3);
+    st.insert(4);
+    st.insert(5);
+
+    auto it = st.find(3);
+
+    auto it = st.find(6);
+
+    st.erase(5)
+
+    int cnt =st.find(1);
+}
