c0aba2e0160d4d69294d9b443971a75568a7fef7
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-03-02 00:06:13 +0530

    #

--- DSA/countdigit.cpp
+++ DSA/countdigit.cpp
@@ -1,7 +1,6 @@
 // Count Digits
 // Difficulty: EasyAccuracy: 30.45%Submissions: 438K+Points: 2
 // Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a remainder). Return the total number of such digits.
-
 // A digit d of n divides n evenly if the remainder when n is divided by d is 0 (n % d == 0).
 // Digits of n should be checked individually. If a digit is 0, it should be ignored because division by 0 is undefined.
 
