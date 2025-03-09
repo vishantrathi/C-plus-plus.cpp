40b58f8d202ff7f4c9798e10edda7c22baacc873
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-03-09 19:19:23 +0530

    #

--- DSA/recurtion.cpp
+++ DSA/recurtion.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
+#include <bits/stdc++>
 using namespace std;
 
-int factorial(int n) {
-    if (n == 0) // Base case
-        return 1;
-    else
-        return n * factorial(n - 1); // Recursive call
-}
+// int factorial(int n) {
+//     if (n == 0)
+//         return 1;
+//     else
+//         return n * factorial(n - 1);
+// }
 
-int main() {
-    int num;
-    cout << "Enter a positive integer: ";
-    cin >> num;
-    cout << "Factorial of " << num << " is " << factorial(num) << endl;
-    return 0;
-}
+// int main() {
+//     int num;
+//     cout << "Enter a positive integer: ";
+//     cin >> num;
+//     cout << "Factorial of " << num << " is " << factorial(num) << endl;
+//     return 0;
+// }
+
+// Recurtion : when a function calls itself. it is called recurtion.
+
