60c5d7df2bcbbf8f77c49c7f2b8c5eae009ea637
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-03-15 18:08:35 +0530

    #

---
+++ DSA/Recurtion/PrintLinearlyFrom.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printlinearly(int i, int n) {
+    if (i < 1) { 
+        return;
+    }
+    printlinearly(i - 1, n); 
+    cout << i << " "; 
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printlinearly(n - 1, n); 
+    return 0;
+}
