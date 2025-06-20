2ffc744b50731fa6cae12b8e4a3c8b6ba3a10502
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-06-20 23:49:36 +0530

    Day 1 of the DSA

---
+++ DSA/Basics of DS/DataTypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num =2;
+    char character='A';
+    bool boolean=true;
+    float floatnum=3.14;
+    double doublenum=3.14159;
+
+    cout<<sizeof(int) <<" the num is "<<num<<endl;
+    cout<<sizeof(char) <<" the character is "<<character<<endl;
+    cout<<sizeof(bool) <<" the boolean is "<<boolean<<endl;
+    cout<<sizeof(float) <<" the float is "<<floatnum<<endl;
+    cout<<sizeof(double) <<" the double is "<<doublenum<<endl;
+
+}
