34f089cb1dae22db0757bf5f84fe362752ea16c7
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-07-05 08:59:17 +0530

    day7

--- DSA/Basics of DS/pattern.cpp
+++ DSA/Basics of DS/pattern.cpp
@@ -33,20 +33,40 @@ int main(){
     // }
 
     //Pattern 3
-    int n;
-    cin>>n;
+    // int n;
+    // cin>>n;
 
-    int i=1;
-    int count =1;
-    while (i<=n){
-        int j=1;
-        while (j<=n){
-            cout<<count;
-            count=count+1;
-            j=j+1;
+    // int i=1;
+    // int count =1;
+    // while (i<=n){
+    //     int j=1;
+    //     while (j<=n){
+    //         cout<<count<<" ";
+    //         count=count+1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+
+    //Pattern 4
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    int count=1;
+    int row=1;
+        while(row<=n){
+            int col=1;
+            
+            while(col<=row){
+                cout<<" "<<count;
+                count++;
+                col=col+1;
+             
+            }
+            cout<<endl;
+            row=row+1;
+
         }
-        cout<<endl;
-        i=i+1;
     }
-    
-}
