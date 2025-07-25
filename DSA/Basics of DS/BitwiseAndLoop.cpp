fc108a2eabb2fe679dbe283ac6fa86f6eb1c43ee
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-07-25 23:51:13 +0530

    commit

--- DSA/Basics of DS/BitwiseAndLoop.cpp
+++ DSA/Basics of DS/BitwiseAndLoop.cpp
@@ -1,14 +1,33 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter a number"<<endl;
-    cin>>n;
+    // int n;
+    // cout<<"Enter a number"<<endl;
+    // cin>>n;
 
-    int sum=0;
+    // int sum=0;
 
-    for(int i=1; i<=n; i++){
-        sum+=i;
-    }
-    cout<<sum<<endl;
-}
+    // for(int i=1; i<=n; i++){
+    //     sum+=i;
+    // }
+    // cout<<sum<<endl;
+
+    // int a=4;
+    // int b=6;
+    // cout<<"a&b "<<(a&b)<<endl;
+    // cout<<"a|b "<<(a|b)<<endl;
+    // cout<<"~a "<<(~a)<<endl;
+    // cout<<"a^b "<<(a^b)<<endl;
+
+    // cout<<a++;
+    // cout<<++a;
+    // cout<<a--;
+    // cout<<--a;
+
+    //for loop
+
+    
+
+
+
+} 
--- DSA/Basics of DS/pattern.cpp
+++ DSA/Basics of DS/pattern.cpp
@@ -137,11 +137,17 @@ int main(){
 
     int n;
     cin>>n;
-    
-    
-
-
-
-
-    
+    int row =1;
+    while(row<=n){
+        int space =n-row;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+        int col=1;
+        cout<<"*";
+        col=col+1;
+    }
+    cout<<endl;
+    row=row+1;
 }
