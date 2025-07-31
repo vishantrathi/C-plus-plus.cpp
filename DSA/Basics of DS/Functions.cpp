053e7ddf01a2c1e0dde31a8fc305369574e599c8
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-07-31 23:25:10 +0530

    commit

--- DSA/Basics of DS/Functions.cpp
+++ DSA/Basics of DS/Functions.cpp
@@ -31,15 +31,42 @@ using namespace std;
 //     }
 // }
 
-int factorial(int n){
-    int fact =1;
-    for(int i=1; i<=n;i++){
-        fact=fact*i;
-    }
-    return fact;
-}
+// int fact(int n){
+//     int fact =1;
+//     for(int i=1; i<=n;i++){
+//         fact=fact*i;
+//     }
+//     return fact;
+// }
 
-int ncr(int n,int r){
-    int num =fact(n);
-    int denom=
-}
+// int nCr(int n,int r){
+//     int num =fact(n);
+//     int denom=fact(r)*fact(n-r);
+//     int ans=num/denom;
+//     return ans;
+// }
+
+// int main( ) {
+
+//     int n, r;
+
+//     cin>> n >> r ;
+
+//     cout <<" Answer is " << nCr(n,r) << endl;
+
+//     return 0;
+// }
+
+// counting function
+// void printcounting(int n){
+//     for(int i=0;i<=n;i++){
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     printcounting(n);
+//     return 0;
+// }
