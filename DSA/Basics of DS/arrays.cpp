8e403a8dcb2cbc1e033a8184969b2e32046a584f
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-08-02 00:51:42 +0530

    commit

--- DSA/Basics of DS/arrays.cpp
+++ DSA/Basics of DS/arrays.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+//An array is a collection of elements of the same type placed in contiguous memory locations.
 int main(){
+    int arr[]{1,2,3,4,5,6,7}; //print garbage value for left element which does not initialized.
+
+//    for (int i = 0; i < 10; i++) {
+//     cout << arr[i] << " ";
     
+//    }
+//    cout<<endl;
+//    cout << arr[1] << " "<<endl; // print element at index 1;
+//    cout << sizeof(arr)<<endl;
+
+
 }
