f34eb879d1051693b81b91fbbf25a704b7fe6b2b
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-08-10 23:55:39 +0530

    commit

--- DSA/Basics of DS/arrays.cpp
+++ DSA/Basics of DS/arrays.cpp
@@ -16,35 +16,34 @@
 #include<climits> // For INT_MIN and INT_MAX
 using namespace std;
 
-int getMax(int num[], int n) {
-    int maxi = INT_MIN;
-    for(int i = 0; i < n; i++) {
-        if(num[i] > maxi) {
-            maxi = num[i];
-        }
-    }
-    return maxi;
-}
+// int getMax(int num[], int n) {
+//     int maxi = INT_MIN;
+//     for(int i = 0; i < n; i++) {
+//         if(num[i] > maxi) {
+//             maxi = num[i];
+//         }
+//     }
+//     return maxi;
+// }
 
-int getMin(int num[], int n) {
-    int mini = INT_MAX;
-    for(int i = 0; i < n; i++) {
-        if(num[i] < mini) {
-            mini = num[i];
-        }
-    }
-    return mini;
-}
+// int getMin(int num[], int n) {
+//     int mini = INT_MAX;
+//     for(int i = 0; i < n; i++) {
+//         if(num[i] < mini) {
+//             mini = num[i];
+//         }
+//     }
+//     return mini;
+// }
 
-int main() {
-    int size;
-    cin >> size;
-    int num[1000]; // Assuming max size is 1000
+// int main() {
+//     int size;
+//     cin >> size;
+//     int num[1000];
 
-    for(int i = 0; i < size; i++) {
-        cin >> num[i];
-    }
-    cout << "Minimum value is " << getMin(num, size) << endl;
-
-    return 0;
-}
+//     for(int i = 0; i < size; i++) {
+//         cin >> num[i];
+//     }
+//     cout << "Minimum value is " << getMin(num, size) << endl;
+//     return 0;
+// }
