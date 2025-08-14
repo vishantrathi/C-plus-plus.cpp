f97d99529934738807d532083c2f305b0f99aaf8
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-08-14 23:53:00 +0530

    commit

---
+++ DSA/Basics of DS/LeetCode/question6.cpp
@@ -0,0 +1,22 @@
+// Odd or Even
+
+// Given a positive integer n, determine whether it is odd or even. Return true if the
+// number is even and false if the number is odd.
+// Examples:
+// Input: n —
+// - 15
+// Output: false
+// Explanation: The number is not divisible by 2,
+// Input: n = 44
+// Output: true
+// Odd number.
+// Explanation: The number is divisible by 2, Even number.
+
+
+class Solution {
+public : 
+bool isEven(int n) {
+// code here
+if
+return true;
+return false;
---
+++ DSA/Basics of DS/TimeAndComplexity.cpp
@@ -0,0 +1,17 @@
+// It is the amount of time taken by an algorithm to run. 
+// as a function of length of the input.
+
+// slow-->high_processing--->Algo A---->fast machine---->fast algo
+// fast-->low_processing--->Algo B---->slow machine---->slow algo
+
+// Big Oh
+// Theta Oh
+// Omega Oh
+
+constant time = o(1)
+linear time= o(n)
+log time=0(logn)
+quadatic time= o(n2)
+cubic time=o(n3)
+
+
