7316f7fd7de39a9f985d1f14f8f730c4f86eba1c
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-08-20 23:55:02 +0530

    commit

---
+++ DSA/Basics of DS/selectionSort.cpp
@@ -0,0 +1,5 @@
+// Selection Sort is a comparison-based sorting algorithm. It sorts an array by repeatedly selecting the smallest (or largest) element from the unsorted portion and swapping it with the first unsorted element. This process continues until the entire array is sorted.
+
+// First we find the smallest element and swap it with the first element. This way we get the smallest element at its correct position.
+// Then we find the smallest among remaining elements (or second smallest) and swap it with the second element.
+// We keep doing this until we get all elements moved to correct position.
