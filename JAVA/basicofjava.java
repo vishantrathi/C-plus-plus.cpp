e9c8ba5ba27b3ba1339e5292d08e59abce61fb17
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-09-27 22:41:57 +0530

    commit

--- JAVA/basicofjava.java
+++ JAVA/basicofjava.java
@@ -1,7 +1,8 @@
-public class basicofjava{ //public class , public class and file name should be the same
-    public static void main(String args[]){  //main function
-
+public class basicofjava {
+    public static void main(String[] args) {
+        System.out.print("1 2 3 4 5");
     }
 }
+
 
 // above all is the boiler plate code
