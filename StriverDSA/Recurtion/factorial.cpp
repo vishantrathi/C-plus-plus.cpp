fd2b863fcfbfff92c5c1dc153dcbb65566378d29
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2026-03-05 23:09:05 +0530

    commit

---
+++ StriverDSA/Recurtion/factorial.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n==0) return 1;
+    return n* fact(n-1);
+}
+
+int main(){
+    int n=3;
+    cout<<fact(n);
+    return 0;
+}
---
+++ StriverDSA/Recurtion/q4.cpp
@@ -0,0 +1,1 @@
+// print 1 to n using backtracking
