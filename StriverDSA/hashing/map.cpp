56287cf1ece4c8a5f1084b1c9f76b826b93c9317
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2026-03-08 18:31:09 +0530

    commit

---
+++ StriverDSA/hashing/map.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    //pre-compute
+
+    map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+
+    //iterate the map
+
+    for(auto it:mpp){
+        // cout<<it.first<<" "<<it.second<<endl;
+    }
+    int q;
+    cin>>q;  //nos of query you want to made
+    while(q--){
+        int number;
+        cin>>number;
+        //fetch
+        cout<<mpp[number]<<endl;
+    }
+    return 0;
+}
